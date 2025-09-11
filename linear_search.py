05dd7a1a038f9e7fae3dea1772b49a8e8936d1e2
Ana Luiz <acluiz.study@gmail.com>
2025-09-11 20:54:32 -0300

    refactor: change numbers list

--- linear_search.py
+++ linear_search.py
@@ -1,26 +1,23 @@
-
 def linear_search(list, target):
-    runs = 0
-    
-    for index in range(0,len(list)):
-        runs = runs + 1
+  runs = 0
 
-        if list[index] == target:
-            print("Total runs: ", runs)
-            return index
-    
-    print("Total runs: ", len(list))
-    return None
+  for index in range(0, len(list)):
+    runs = runs + 1
+
+    if list[index] == target:
+      print("Total runs: ", runs)
+      return index
+
+  print("Total runs: ", len(list))
+  return None
 
 def log_target_position(target_index):
-    if target_index is not None:
-        print("Target found at index: ", target_index)
-    else:
-        print("Target not found")
+  if target_index is not None:
+    print("Target found at index: ", target_index)
+  else:
+    print("Target not found")
 
-
-
-numbers = [3, 7, 12, 18, 21, 25, 32, 38, 41, 47, 53, 59, 62, 70, 74, 81, 85, 90, 96]
-target_index = linear_search(numbers, 38)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target_index = linear_search(numbers, 9)
 
 log_target_position(target_index)
